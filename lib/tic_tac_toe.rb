72c9ab90355c16822d91dbf21df429f3a96bb74b
Michael Schneider <mic.sch91@gmail.com>
2020-04-14 13:47:12 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,7 +52,7 @@ end
     input = gets.strip
     index = input_to_index(input)
     if valid_move?(board, index)
-      move(index)
+      move(board, index)
       display_board(board)
     else
       turn(board)
